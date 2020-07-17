65e45ee7ac9401e6404b01a135cddea63a6285be
Mo Young <>
2020-07-17 23:26:38 +0800

    LeetCode 258: 各位相加

--- Week_02/Day12/258_add_digits.py
+++ Week_02/Day12/258_add_digits.py
@@ -4,9 +4,5 @@ leetcode: 258
 
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num == 0:
-            return 0
-        if num % 9 == 0:
-            return 9
-        else:
-            return num % 9
+        if num == 0: return 0
+        return (num - 1) % 9 + 1

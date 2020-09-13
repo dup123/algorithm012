41a98d5c150c0b2ea1c68e5d3f91b3d1910a6283
杜埔 <505663339@qq.com>
2020-09-13 22:46:52 +0800

    第9周 作业提交 课程精讲题目+课后作业+学习心得

---
+++ Week_09/[76]最小覆盖子串.java
@@ -0,0 +1,70 @@
+//给你一个字符串 S、一个字符串 T 。请你设计一种算法，可以在 O(n) 的时间复杂度内，从字符串 S 里面找出：包含 T 所有字符的最小子串。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：S = "ADOBECODEBANC", T = "ABC"
+//输出："BANC" 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 如果 S 中不存这样的子串，则返回空字符串 ""。 
+// 如果 S 中存在这样的子串，我们保证它是唯一的答案。 
+// 
+// Related Topics 哈希表 双指针 字符串 Sliding Window 
+// 👍 745 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+    public String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
